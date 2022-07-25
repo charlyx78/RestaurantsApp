54741a3aaa9eafbf05385203fa25a190c7753d02
Gilberto Lopez <gilbert.quint@hotmail.com>
2022-07-25 11:50:39 -0700

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstPageComponent } from './first-page/first-page.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { HomePreparingComponent } from './home-preparing/home-preparing.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,10 @@ import { HomePreparingComponent } from './home-preparing/home-preparing.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ApiService } from '../service.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public results = [];
+  dataImg:any;
+  nameImg: string ='';
+
+  registerRestaurantForm: FormGroup;
+
+  constructor(private api:ApiService, public formBuilder: FormBuilder) {
+    this.registerRestaurantForm = new FormGroup({
+      name:new FormControl(),
+      address: new FormControl(),
+      phone: new FormControl(),
+      type: new FormControl(),
+      lat: new FormControl(),
+      lng: new FormControl(),
+      shortDescription: new FormControl(),
+      longDescription: new FormControl(),
+      openLocal: new FormControl(),
+      closeLocal: new FormControl(),
+
+    })
+   }
 
   ngOnInit(): void {
+  }
+
+  onFile(event: any):void {
+    console.log(event);
+    const[file] = event.target.files;
+    this.dataImg = {
+      fileRaw:file,
+      fileName:file.name
+    }
+    
+  }
+
+  upload(){
+    const data = new FormData();
+    data.append('usrimage',this.dataImg.fileRaw)
+
+    this.api.postImages(data).subscribe((resposeFromTheServer)=>{
+      let resposeLocal;
+      resposeFromTheServer = resposeLocal
+    
+    })
   }
 
 }
